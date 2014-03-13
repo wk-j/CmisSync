b9ad24c42c367a3665d8e13b861eebb65257cd86
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-03-13 14:13:22 +0900

    split delete into own method. don&t update deleted objects

--- CmisSync.Lib/Sync/ChangeLogStrategy.cs
+++ CmisSync.Lib/Sync/ChangeLogStrategy.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System;
 using CmisSync.Lib.Cmis;
+using DotCMIS.Exceptions;
 
 
 namespace CmisSync.Lib.Sync
@@ -87,7 +88,17 @@ namespace CmisSync.Lib.Sync
 
                     // Case when an object has been updated.
                     case ChangeType.Updated:
-                        ICmisObject cmisObject = session.GetObject(change.ObjectId);
+                        ICmisObject cmisObject = null;
+                        try
+                        {
+                            cmisObject = session.GetObject(change.ObjectId);
+                        }
+                        catch (CmisObjectNotFoundException e)
+                        {
+                            Logger.Warn("Sync | Object can not be updated because it can not be found anymore. Deleting it locally: " + change.ObjectId);
+                            Delete(change.ObjectId);
+                            break;
+                        }
                         if (null != (remoteDocument = cmisObject as IDocument))
                         {
                             if (remoteDocument.Paths.Count == 0)
@@ -117,57 +128,7 @@ namespace CmisSync.Lib.Sync
 
                     // Case when an object has been deleted.
                     case ChangeType.Deleted:
-                        Logger.Info("Sync | Deleting locally because deleted on remote server: " + change.ObjectId);
-
-                        string id = change.ObjectId;
-
-                        // Possible bug in Alfresco, see http://stackoverflow.com/q/22294589
-                        if (id.Contains("workspace://SpacesStore/"))
-                        {
-                            id = id.Substring("workspace://SpacesStore/".Length);
-                        }
-
-                        // In the local database, find and remove the file/folder that has this id.
-                        string path = database.RemoveId(id);
-
-                        if (path == null)
-                        {
-                            Logger.Error("Sync | Not deleting because no file/folder found with this object id: " + change.ObjectId);
-                        }
-                        else
-                        {
-                            // Delete the file/folder from the local filesystem.
-                            //string relativeFolderPath = Path.GetDirectoryName(remoteDocumentPath);
-                            string unescapedPath = path.Replace('/', '\\'); // TODO OS-specific separator
-                            unescapedPath = unescapedPath.Substring(repoinfo.Name.Length + 1);
-                            string absolutePath = Path.Combine(repoinfo.TargetDirectory, unescapedPath); // TODO use localFolder instead of repoinfo.TargetDirectory ?
-
-                            if (Utils.IsDirectory(absolutePath))
-                            {
-                                try
-                                {
-                                    Logger.Info("Removing remotely deleted folder recursively: " + absolutePath);
-                                    Directory.Delete(absolutePath, true);
-                                }
-                                catch (Exception e)
-                                {
-                                    ProcessRecoverableException("Could not delete folder recursively:" + absolutePath, e);
-                                }
-                            }
-                            else
-                            {
-                                try
-                                {
-                                    Logger.Info("Removing remotely deleted file: " + absolutePath);
-                                    File.Delete(absolutePath);
-                                }
-                                catch (Exception e)
-                                {
-                                    ProcessRecoverableException("Could not delete file:" + absolutePath, e);
-                                }
-                            }
-                        }
-                        
+                        Delete(change.ObjectId);
                         break;
 
                     // Case when access control or security policy has changed.
@@ -179,6 +140,61 @@ namespace CmisSync.Lib.Sync
                     default:
                         Logger.Warn("Sync | Not applied because change not implemented: " + change.ChangeType);
                         break;
+                }
+            }
+
+
+            private void Delete(string objectId)
+            {
+                Logger.Info("Sync | Deleting locally because deleted on remote server: " + objectId);
+
+                string id = objectId;
+
+                // Possible bug in Alfresco, see http://stackoverflow.com/q/22294589
+                if (id.Contains("workspace://SpacesStore/"))
+                {
+                    id = id.Substring("workspace://SpacesStore/".Length);
+                }
+
+                // In the local database, find and remove the file/folder that has this id.
+                string path = database.RemoveId(id);
+
+                if (path == null)
+                {
+                    Logger.Error("Sync | Not deleting because no file/folder found with this object id: " + objectId);
+                }
+                else
+                {
+                    // Delete the file/folder from the local filesystem.
+                    //string relativeFolderPath = Path.GetDirectoryName(remoteDocumentPath);
+                    string unescapedPath = path.Replace('/', '\\'); // TODO OS-specific separator
+                    unescapedPath = unescapedPath.Substring(repoinfo.Name.Length + 1);
+                    string absolutePath = Path.Combine(repoinfo.TargetDirectory, unescapedPath); // TODO use localFolder instead of repoinfo.TargetDirectory ?
+
+                    if (Utils.IsDirectory(absolutePath))
+                    {
+                        try
+                        {
+                            Logger.Info("Removing remotely deleted folder recursively: " + absolutePath);
+                            Directory.Delete(absolutePath, true);
+                        }
+                        catch (Exception e)
+                        {
+                            ProcessRecoverableException("Could not delete folder recursively:" + absolutePath, e);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Logger.Info("Removing remotely deleted file: " + absolutePath);
+                            File.Delete(absolutePath);
+                        }
+                        catch (Exception e)
+                        {
+                            ProcessRecoverableException("Could not delete file:" + absolutePath, e);
+                        }
+                    }
                 }
             }
         }
